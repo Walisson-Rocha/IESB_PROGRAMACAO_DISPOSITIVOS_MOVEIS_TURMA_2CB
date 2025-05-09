8613d819dab89bb1d81722957703fa18354106f3
Walisson <walissonoficial19@gmail.com>
2025-05-09 20:39:14 -0300

    navegação

--- 10-navegacao-drawer/src/routes/DrawerRoutes.jsx
+++ 10-navegacao-drawer/src/routes/DrawerRoutes.jsx
@@ -1,9 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { createDrawerNavigator } from "@react-navigation/drawer"
+
+import HomeScreen from "../screens/HomeScreen"
+import ProfileScreen from "../screens/ProfileScreen"
+import ConfigScreen from "../screens/ConfigScreen"
+import {Ionicons} from '@expo/vector-icons' 
+
+const DrawerNav = createDrawerNavigator()
 
 export default function DrawerRoutes() {
   return (
+    <DrawerNav.Navigator>
 
+
+      <DrawerNav.Screen
+       name="Home"
+       component={HomeScreen} 
+       options={{
+        title: 'Inicio',
+       }}
+       />
+
+
+      <DrawerNav.Screen name="Profile" component={ProfileScreen} />
+      <DrawerNav.Screen name="Config" component={ConfigScreen} />
+    </DrawerNav.Navigator>
   )
 }
-
--- 10-navegacao-drawer/src/screens/HomeScreen.jsx
+++ 10-navegacao-drawer/src/screens/HomeScreen.jsx
@@ -1,10 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native-paper'
 
-export default function HomeScreen() {
+export default function HomeScreen({navigation, route}) {
   return (
     <View>
       <Text>HomeScreen</Text>
+
+      <Button
+      mode='contained'
+      onPress={() => navigation.operDrawer()}
+      >Abrir gaveta
+      </Button>
+
+      <Button
+      mode='contained-tonal'
+      nPress={ () => {
+        navigation.operDrawer()
+        setTimeout(() => navigation.closeDrawer(), 3000)
+      }}
+      > 
+      Abrir gaveta autom
+      </Button>
+
     </View>
   )
 }
